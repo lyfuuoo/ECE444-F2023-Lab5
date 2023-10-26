0002d0e29a307ce7d6074bc28c49551809fd5490
Yoko <yokofenglee@gmail.com>
2023-10-26 14:53:01 -0400

    fix the sql file error and finish show entries section

--- flaskr-tdd/project/schema.sql
+++ flaskr-tdd/project/schema.sql
@@ -1,8 +1,8 @@
-drop table if exists entries;
---This will set up a single table with three fields: "id", "title", and "text"
+DROP TABLE IF EXISTS entries;
 
+-- This will set up a single table with three fields: "id", "title", and "text"
 CREATE TABLE entries (
-  id integer primary key,
-  title text not null,
-  text text not null,
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  text TEXT NOT NULL
 );
